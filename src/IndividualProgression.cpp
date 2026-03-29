1724f536ebdec9c13aebf08bc730ea63d12e2cfa
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2026-03-29 22:17:07 +0200

    restore hp/absorb aura vanilla/tbc

--- src/IndividualProgression.cpp
+++ src/IndividualProgression.cpp
@@ -106,9 +106,9 @@ void IndividualProgression::AdjustStats(Player* player, float computedPowerAdjus
 	auto bp2 = static_cast<int32>(computedHealthAdjustment);
 	
     player->RemoveAura(HP_AURA_SPELL);
-    //player->CastCustomSpell(player, HP_AURA_SPELL, &bp2, nullptr, nullptr, true);
+    player->CastCustomSpell(player, HP_AURA_SPELL, &bp2, nullptr, nullptr, true);
     player->RemoveAura(ABSORB_SPELL);
-    //player->CastCustomSpell(player, ABSORB_SPELL, &bp1, nullptr, nullptr, true);
+    player->CastCustomSpell(player, ABSORB_SPELL, &bp1, nullptr, nullptr, true);
 }
 
 float IndividualProgression::ComputeVanillaAdjustment(uint8 playerLevel, float configAdjustmentValue)
